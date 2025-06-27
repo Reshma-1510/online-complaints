4b5f0b3fa90a81068185724c59f5e6c4dd0e2b22
Reshma-1510 <manyalareshma1510@gmail.com>
2025-06-27 15:39:48 +0530

    Delete backend directory

--- backend/backend/complaintroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/complaintController');
-
-router.post('/complaints', controller.createComplaint);
-router.get('/complaints', controller.getAllComplaints);
-router.put('/complaints/:id/status', controller.updateStatus);
-
-module.exports = router;
